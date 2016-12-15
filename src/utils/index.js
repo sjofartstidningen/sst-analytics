6a054e7b8912a5cb38aad32ec17a95d2f52b80a0
Adam Bergman <adam@fransvilhelm.com>
2016-12-15 21:20:35 +0100

    ADD capitalizeFirst utility

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,9 @@
 import format from 'date-fns/format';
+import R from 'ramda';
 
 export const formatDate = (date: Date): string => format(date, 'YYYY-MM-DD');
+export const capitalizeFirst = (str: string): string => R.compose(
+  R.join(''),
+  R.over(R.lensIndex(0), R.toUpper),
+  R.split(''),
+)(str);

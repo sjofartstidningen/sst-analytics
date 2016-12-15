be58af379241d7f05e5da62c240547272aa46d87
Adam Bergman <adam@fransvilhelm.com>
2016-12-15 02:04:05 +0100

    UPDATE api/google with more explicit methods

--- src/api/google/index.js
+++ src/api/google/index.js
@@ -1,70 +1,75 @@
 // @flow
 import google from 'googleapis';
 import { promisify } from 'bluebird';
-import format from 'date-fns/format';
+import R from 'ramda';
+
 import auth from './auth';
+import getDateRanges from './getDateRanges';
+import { formatDate } from '../../utils';
+import { GA_ACCOUNT_ID, GA_WEB_PROPERTY, GA_PROFILE_ID, CLIENT_EMAIL, PRIVATE_KEY } from '../../env';
 
-const analytics = google.analytics('v3');
-const analyticsGet = promisify(analytics.data.ga.get);
+const analytics = promisify(google.analytics('v3').data.ga.get);
 
-const formatDate = (date: Date): string => format(date, 'YYYY-MM-DD');
-
-const {
-  GA_ACCOUNT_ID,
-  GA_WEB_PROPERTY,
-  GA_PROFILE,
-  CLIENT_EMAIL,
-  PRIVATE_KEY,
-} = process.env;
-
-if (!GA_ACCOUNT_ID || !GA_WEB_PROPERTY || !GA_PROFILE || !CLIENT_EMAIL || !PRIVATE_KEY) {
-  throw new Error('Necessary environment variables is not set');
-}
-
-const accountId = Number(GA_ACCOUNT_ID);
-const profileId = Number(GA_PROFILE);
-if (isNaN(accountId)) throw new Error('Environment variable GA_ACCOUNT_ID must be a number');
-if (isNaN(profileId)) throw new Error('Environment variable GA_PROFILE must be a number');
-
-type GAdata = {
-  start: Date,
-  end: Date,
-  metrics: string,
-  dimensions?: string,
-  sort?: string,
-};
-
-export default async ({ start, end, metrics, dimensions, sort }: GAdata): Promise<any> => {
-  let jwt;
-
+export const coreApi = async ({ start, end, metrics, dimensions, sort }: GAdata): Promise<any> => {
   try {
-    jwt = await auth(CLIENT_EMAIL, PRIVATE_KEY);
+    const payload: GApayload = {
+      auth: await auth(CLIENT_EMAIL, PRIVATE_KEY),
+      accountId: GA_ACCOUNT_ID,
+      profileId: GA_PROFILE_ID,
+      webPropertyId: GA_WEB_PROPERTY,
+      ids: `ga:${GA_PROFILE_ID}`,
+      'start-date': formatDate(start),
+      'end-date': formatDate(end),
+      'max-results': 50,
+      metrics,
+    };
+
+    if (dimensions) payload.dimensions = dimensions;
+    if (sort) payload.sort = sort;
+
+    const result = await analytics(payload);
+    return result;
   } catch (err) {
     throw err;
   }
+};
 
-  const payload: GApayload = {
-    auth: jwt,
-    accountId,
-    webPropertyId: GA_WEB_PROPERTY,
-    profileId,
-    ids: `ga:${GA_PROFILE}`,
-    'start-date': formatDate(start),
-    'end-date': formatDate(end),
-    'max-results': 20,
-    metrics,
-  };
+// buildMetricsString :: <String> => String
+const buildMetricsString = R.compose(R.join(','), R.map(R.concat('ga:')));
 
-  if (dimensions) payload.dimensions = dimensions;
-  if (sort) payload.sort = sort;
+// redefineKeys :: { k: v } => { k: v }
+const redefineKeys = R.compose(R.fromPairs, R.map(R.adjust(R.replace('ga:', ''), 0)), R.toPairs);
 
-  let result;
+// extractData :: <{a: b}> => <{c: d}>;
+const extractData = R.map(R.compose(redefineKeys, R.prop('totalsForAllResults')));
 
+type GAmetrics = 'sessions' | 'pageviews' | 'users';
+export const getViewMetrics = async (...metricsArr: Array<GAmetrics>): Promise<any> => {
   try {
-    result = await analyticsGet(payload);
+    const metrics = buildMetricsString(metricsArr);
+    const dateRanges = getDateRanges(new Date());
+
+    const promises = dateRanges.map(({ end, start }) => coreApi({ end, start, metrics }));
+    const result = await Promise.all(promises);
+    const data = extractData(result);
+    return data;
   } catch (err) {
     throw err;
   }
-
-  return result;
 };
+
+//   const mostViews = await getMostViews({
+//     start,
+//     end,
+//     metrics: 'ga:pageviews',
+//     dimensions: 'ga:pagePath,ga:pageTitle',
+//     sort: '-ga:pageviews',
+//   });
+//
+//   const referrals = await getReferrals({
+//     start,
+//     end,
+//     metrics: 'ga:sessions',
+//     dimensions: 'ga:source',
+//     sort: '-ga:sessions',
+//   });

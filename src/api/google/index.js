990399dfb171389c25bfe6384f553990260c40cc
Adam Bergman <adam@fransvilhelm.com>
2016-12-16 10:40:14 +0100

    UPDATE environment variables names

--- src/api/google/index.js
+++ src/api/google/index.js
@@ -8,8 +8,8 @@ import env from '../../env';
 
 const analytics = promisify(google.analytics('v3').data.ga.get);
 
-const CLIENT_EMAIL = env('CLIENT_EMAIL');
-const PRIVATE_KEY = env('PRIVATE_KEY');
+const GA_CLIENT_EMAIL = env('GA_CLIENT_EMAIL');
+const GA_PRIVATE_KEY = env('GA_PRIVATE_KEY');
 const GA_ACCOUNT_ID = env('GA_ACCOUNT_ID');
 const GA_PROFILE_ID = env('GA_PROFILE_ID');
 const GA_WEB_PROPERTY = env('GA_WEB_PROPERTY');
@@ -19,7 +19,7 @@ export default async (
 ): Promise<any> => {
   try {
     const payload: GApayload = {
-      auth: await auth(CLIENT_EMAIL, PRIVATE_KEY),
+      auth: await auth(GA_CLIENT_EMAIL, GA_PRIVATE_KEY),
       accountId: Number(GA_ACCOUNT_ID),
       profileId: Number(GA_PROFILE_ID),
       webPropertyId: GA_WEB_PROPERTY,

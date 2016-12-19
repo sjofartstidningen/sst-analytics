70bdfdda297d5f261bf2b28e939b1eebd065f133
Adam Bergman <adam@fransvilhelm.com>
2016-12-20 00:45:34 +0100

    EXPORT only coreApi as default

--- src/api/mailchimp/index.js
+++ src/api/mailchimp/index.js
@@ -1,23 +1,15 @@
 // @flow
-import R from 'ramda';
 import axios from 'axios';
 import env from '../../env';
-import { baseUrl, reportUrl } from './generateUrl';
+import { baseUrl } from './generateUrl';
 
 const MC_KEY = env('MC_KEY');
 const MC_USER = env('MC_USER');
 
-export const coreApi = axios.create({
+export default axios.create({
   baseURL: baseUrl(),
   auth: {
     username: MC_USER,
     password: MC_KEY,
   },
 });
-
-export const getReport = async (campaignId: string): Promise<MCreport> => {
-  const result = await coreApi(reportUrl(campaignId));
-  const data: MCreport = result.data;
-
-  return data;
-};

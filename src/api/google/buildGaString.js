6bad84b48f4380345131587dc29e5076e364ee78
Adam Bergman <adam@fransvilhelm.com>
2016-12-20 20:41:58 +0100

    REMOVE obsolete

--- src/api/google/buildGaString.js
+++
@@ -1,6 +0,0 @@
-import R from 'ramda';
-
-// buildGaString :: [String] => String
-export default (metricsArr: Array<GAmetrics>) => (
-  R.compose(R.join(','), R.map(R.concat('ga:')))(metricsArr)
-);

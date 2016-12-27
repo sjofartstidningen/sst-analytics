41d09710abd874ffdab3164b326f9586e2cd0130
Adam Bergman <adam@fransvilhelm.com>
2016-12-27 13:35:31 +0100

    ADD module transformPug
    transformPug will translate a report into an MJML template string using template language Pug.
    This string can than be transformed into a responsive email using MJML.

---
+++ src/template/template.spec.js
@@ -0,0 +1,39 @@
+import test from 'blue-tape';
+import cheerio from 'cheerio';
+import generateEmail from '../template';
+import transformPug from './transformPug';
+import report from '../../test/report-example.json';
+
+test.skip('Template: generateEmail()', (t) => {
+  generateEmail();
+  t.end();
+});
+
+test('Template: transformPug()', async (t) => {
+  const result = await transformPug(report);
+  const $ = cheerio.load(result);
+
+  {
+    const should = 'Should generate an MJML template from Pug with 9 <mj-section>';
+    const actual = $('mj-section').length;
+    const expected = 9;
+
+    t.equal(actual, expected, should);
+  }
+
+  {
+    const should = 'Should generate correct data';
+    const actual = [
+      $('mj-text[mj-class="header subheader"]').text().trim(),
+      $('mj-text[mj-class="list-text"]').first().html().trim(),
+      $('mj-text[mj-class="list-text"]').last().html().trim(),
+    ];
+    const expected = [
+      'vecka 50 – 2016',
+      '18.323<span class="label red">-1,3%</span>',
+      '12,8%',
+    ];
+
+    t.deepEqual(actual, expected, should);
+  }
+});
---
+++ src/template/transformPug.js
@@ -0,0 +1,39 @@
+import { join } from 'path';
+import { renderFile } from 'pug';
+import { promisify } from 'bluebird';
+import numeral from 'numeral';
+import { truncate, capitalize } from 'lodash';
+
+const renderFileAsync = promisify(renderFile);
+
+numeral.register('locale', 'sv', {
+  delimiters: { thousands: '.', decimal: ',' },
+  abbreviations: { thousand: 'k', million: 'm', billion: 'b', trillion: 't' },
+  ordinal: number => number,
+  currency: { symbol: 'SEK' },
+});
+
+numeral.locale('sv');
+
+export default async (data) => {
+  const templatePath = join(__dirname, 'template.pug');
+  const opts = Object.assign({}, {
+    pretty: true,
+    formatNum: num => numeral(num).format('0,0'),
+    formatPerc: num => numeral(num).format('+0,0.0%'),
+    formatPercNoSign: num => numeral(num).format('0,0.0%'),
+    truncateTitle: str => truncate(str, { length: 33 }),
+    capitalize,
+    getRelativeUrl: (fullUrl) => {
+      const relativeUrl = fullUrl.replace('www.sjofartstidningen.se', '');
+      return truncate(relativeUrl, { length: 33 });
+    },
+  }, data);
+
+  try {
+    const rendered = await renderFileAsync(templatePath, opts);
+    return rendered;
+  } catch (err) {
+    throw err;
+  }
+};

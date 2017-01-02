c5a833454de59e76669896cc1899ca2f1ac23109
Adam Bergman <adam@fransvilhelm.com>
2017-01-02 12:26:03 +0100

    CREATE handler function

--- handler.js
+++ handler.js
@@ -1,16 +1,8 @@
 'use strict';
+const createAndSend = require('./lib').default;
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.run = (event, context, callback) => {
+  createAndSend()
+    .then(res => callback(null, res))
+    .catch(err => callback(err));
 };

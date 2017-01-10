d7c33e49fba668e64b2865b31c3c4d6ff7529930
Adam Bergman <adam@fransvilhelm.com>
2017-01-10 02:28:24 +0100

    FIX env issue

--- scripts/before-test.sh
+++ scripts/before-test.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -ev
 
-BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
-NODE_ENV="development"
-
-if [[ $BRANCH == 'master' ]]; then
-  NODE_ENV="production"
-fi
-
 echo "Build .env"
 node scripts/build-dotenv.js

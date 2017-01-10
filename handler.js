ea2e67317893a6119766fef9e8ea8bf19766f2a1
Adam Bergman <adam@fransvilhelm.com>
2017-01-10 01:17:40 +0100

    UPDATING handler.send function

--- handler.js
+++ handler.js
@@ -2,9 +2,12 @@ require('dotenv').config({ silent: true });
 const createAndSend = require('./lib/createAndSend');
 
 module.exports.send = (event, context, callback) => {
-  const today = new Date(event.time);
+  const today = new Date();
 
   createAndSend(today)
-    .then(res => callback(null, res))
+    .then((res) => {
+      const ret = Object.assign({}, res, { date: today });
+      return callback(null, ret);
+    })
     .catch(err => callback(err));
 };
